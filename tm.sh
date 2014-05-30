43369695436d2adb913fa070f6bce965dfb36e98
Jon Yurek <jyurek@thoughtbot.com>
2014-05-30 06:17:14 -0700

    Use tmux switch-client -l for toggling
    Ok, so that was dumb. @calebthompson let me know about
    `tmux switch-client -l` which does the same thing as the empty `tm` command.
    
    *ahem* So I changed that.

--- tm.sh
+++ tm.sh
@@ -1,24 +1,17 @@
 #!/bin/sh
 tm() {
   if [ -z $1 ]; then
-    target_session=$(cat /tmp/previous-tmux-session)
-    tm $target_session
+    tmux switch-client -l
   else
     if [ -z "$TMUX" ]; then
       tmux new-session -As $1
     else
-      record_previous_session
       if ! tmux has-session -t $1 2>/dev/null; then
         TMUX= tmux new-session -ds $1
       fi
       tmux switch-client -t $1
     fi
   fi
-}
-
-record_previous_session() {
-  current_session=$(tmux list-sessions | grep '(attached)$' | cut -d: -f 1)
-  echo $current_session > /tmp/previous-tmux-session
 }
 
 tm $1
