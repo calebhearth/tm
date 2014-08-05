91ab8091cf2ddc952b03b3625343b816eb8dd5ca
Caleb Thompson <caleb@calebthompson.io>
2014-08-04 22:12:57 -0500

    Infer working directory if session name in CDPATH
    Automatically set the working directory (the directory windows in a
    session begin in) of new sessions to project folders, assuming that they
    live in ~/code. The cdpath portion could be removed if cdpath were
    already set to something other than `.`.
    
    Using CDPATH necessitated switching the script to zsh instead of sh.

--- tm.sh
+++ tm.sh
@@ -1,12 +1,25 @@
-#!/bin/sh
+#!/bin/zsh
+
 tm() {
   if [ -z $1 ]; then
     tmux switch-client -l
-  else
-    if [ -z "$TMUX" ]; then
-      tmux new-session -As $1
+    exit
+  fi
+
+  local WORKING_DIRECTORY=$(cdpath=(. ~/code) cd $1 > /dev/null 2>&1 && pwd)
+  if [ -z "$TMUX" ]; then
+    if [ -n "$WORKING_DIRECTORY" ]; then
+      tmux new-session -As $1 -c $WORKING_DIRECTORY
     else
-      if ! tmux has-session -t $1 2>/dev/null; then
+      tmux new-session -As $1 -c $HOME
+    fi
+  else
+    if tmux has-session -t $1 2> /dev/null; then
+      tmux switch-client -t $1
+    else
+      if [ -n "$WORKING_DIRECTORY" ]; then
+        TMUX= tmux new-session -ds $1 -c $WORKING_DIRECTORY
+      else
         TMUX= tmux new-session -ds $1
       fi
       tmux switch-client -t $1
